f35bcff8904b1898a559839f30dc843d97ac9455
Your Name <you@example.com>
2018-08-09 16:22:45 +0800

    scr3

---
+++ js/list.js
@@ -0,0 +1,19 @@
+window.onload=function(){
+    let movePic=document.querySelector(".movePic");
+    let include=document.querySelector(".include")
+    let kuan=include.offsetWidth;  
+    let moveImg=document.querySelectorAll(".moveImg");
+    let width=moveImg[0].offsetWidth;
+    let t=setInterval(move,1000)
+    function move(){
+        let now=moveImg[0].style.left=0;
+        let num=0;
+        for(let i=0;i<moveImg.length;i++){
+            for(let j=0;j<move.length;j++){
+                
+            }
+        }
+    }
+    
+    
+}
